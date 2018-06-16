5e9996706dd4c2ec0df168e2baa3be9b9819984d
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-06-16 21:40:19 +0700

    update main path render to index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const Router = require('koa-router')
 
 const router = new Router()
 
-router.get('/', ctx => {
-  ctx.body = `Welcome`
+router.get('/',async ctx => {
+  await ctx.render('index')
 })
 
 router.use(require('./auth'))
