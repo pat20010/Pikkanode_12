c096c026cd6c8f294c7203bf9d1a8ae6435ba92f
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-06-16 16:34:01 +0700

    Add image icon and pikkanode css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 const Koa = require('koa')
+const render = require('koa-ejs')
 const router = require('./routes/index')
+const serve = require('koa-static')
+const path = require('path')
 
 const app = new Koa()
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'template',
+  viewExt: 'ejs',
+  cache: false
+})
+
+app.use(serve(path.join(__dirname, 'public')))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
