fa6f8a37a04801b8cbe646b852dad1da7123156e
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-06-17 16:40:58 +0700

    update users model add find by email

--- src/models/users.js
+++ src/models/users.js
@@ -18,13 +18,18 @@ module.exports = {
     return rows
   },
 
+  async findByEmail (pool, email) {
+    const [rows] = await pool.query(`SELECT * FROM users WHERE email = ?`, [email])
+    return rows
+  },
+
   async removeById (pool, id) {
     const [rowsRemove] = await pool.query(`DELETE FROM users WHERE id = ?`, [id])
     return rowsRemove
   },
 
   async insertUsers (pool, email, password) {
-    const [rowInsert] = await pool.query(`INSERT INTO users (email, password) VALUES (?, ?)`, [email, password])
+    const rowInsert = await pool.query(`INSERT INTO users (email, password) VALUES (?, ?)`, [email, password])
     return rowInsert
   }
 }
