23e57ce114d22eff3129e81d36bb1a833f36cc47
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-06-17 15:16:50 +0700

    add users model

---
+++ src/models/users.js
@@ -0,0 +1,30 @@
+module.exports = {
+  async createEntity (row) {
+    if (!row.id) { return {} }
+
+    return {
+      id: row.id,
+      name: row.name
+    }
+  },
+
+  async findAll (pool) {
+    const [rows] = await pool.query(`SELECT * FROM users`)
+    return rows
+  },
+
+  async findById (pool, id) {
+    const [rows] = await pool.query(`SELECT * FROM users WHERE id = ?`, [id])
+    return rows
+  },
+
+  async removeById (pool, id) {
+    const [rowsRemove] = await pool.query(`DELETE FROM users WHERE id = ?`, [id])
+    return rowsRemove
+  },
+
+  async insertUsers (pool, email, password) {
+    const [rowInsert] = await pool.query(`INSERT INTO users (email, password) VALUES (?, ?)`, [email, password])
+    return rowInsert
+  }
+}
