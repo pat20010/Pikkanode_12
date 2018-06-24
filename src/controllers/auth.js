38b1edd6e30cdea2df68882e89f50d40b6fdfb66
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-06-24 23:55:46 +0700

    update auth controller move check sign in and sign up to service auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,7 @@
 const mysqlError = require('mysql2/lib/constants/errors')
 const auth = require('../service/auth')
-const { users } = require('../models')
 
 async function signInGetHandler (ctx) {
-  console.log(`Flash : ` + ctx.flash)
   const data = {
     flash: ctx.flash
   }
@@ -16,59 +14,46 @@ async function signInPostHandler (ctx) {
   const { email, password } = ctx.request.body
 
   try {
-    const success = await auth.signIn(email, password)
-    console.log(`Sign in status : ${success}`)
+    const userId = await auth.signIn(email, password)
 
-    if (!success) {
-      ctx.session.flash = { error: 'Invalid email or password' }
-      ctx.redirect('/signin')
-      return
-    }
-
-    const dataUser = await users.getUserDataByEmail(email)
-
-    if (!dataUser) {
-      ctx.session.flash = { error: 'Invalid user not found' }
-      ctx.redirect('/signin')
-      return
-    }
-
-    ctx.session.userId = dataUser[0].id
+    ctx.session.userId = userId
     ctx.redirect('/')
   } catch (err) {
+    console.error(err)
+
     ctx.session.flash = { error: err.message }
     ctx.redirect('/signin')
   }
 }
 
 async function signUpGetHandler (ctx) {
-  await ctx.render('/signup')
+  const data = {
+    flash: ctx.flash
+  }
+
+  console.log(data)
+  await ctx.render('/signup', data)
 }
 
 async function signUpPostHandler (ctx) {
   const { email, password } = ctx.request.body
 
   try {
-    const success = await auth.signUp(email, password)
-    console.log(`Sign up status : ${success}`)
-
-    if (!success) {
-      ctx.redirect('/signup')
-      return
-    }
+    await auth.signUp(email, password)
+    console.log(`Sign up status : success`)
 
     ctx.redirect('/signin')
   } catch (err) {
-    console.log(err)
+    console.error(err)
 
     switch (err.errno) {
       case mysqlError.ER_DUP_ENTRY:
-        ctx.status = 400
-        ctx.body = `Email already used`
+        ctx.session.flash = { error: `Email already used` }
+        ctx.redirect('/signup')
         break
       default:
-        ctx.status = 500
-        ctx.body = `Server Error`
+        ctx.session.flash = { error: `Server error` }
+        ctx.redirect('/signup')
     }
   }
 }
