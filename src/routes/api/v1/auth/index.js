4cbd0f6fdc3f7919c91f3fa3e54f62c29c11744e
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-06-24 16:07:18 +0700

    delete images

---
+++ src/routes/api/v1/auth/index.js
@@ -0,0 +1,11 @@
+// const Router = require('koa-router')
+
+// const router = new Router()
+
+// router.post('/signup', auth.signInGetHandler)
+// router.post('/signin', koaBody(), auth.signInPostHandler)
+
+// router.get('/signup', authAlready, auth.signUpGetHandler)
+// router.post('/signup', koaBody(), auth.signUpPostHandler)
+
+// module.exports = router.routes()
