534e1ab60e269f3ca7219ae235946ff199f13fc4
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-06-26 23:27:03 +0700

    add koa body

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,7 +1,8 @@
 const Router = require('koa-router')
+const koaBody = require('koa-body')
 
 const router = new Router()
 
-router.use(require('./auth'))
+router.use(koaBody(), require('./auth'))
 
 module.exports = router.routes()
