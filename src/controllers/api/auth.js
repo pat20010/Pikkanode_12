ec520186dc8ca8e0e3d9e6103fbeda12b9cdf17e
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-06-26 23:28:08 +0700

    update api auth controller func sign in

--- src/controllers/api/auth.js
+++ src/controllers/api/auth.js
@@ -1,14 +1,33 @@
+const auth = require('../../service/auth')
 
 async function signIn (ctx) {
-  ctx.body = {}
+  const { email, password } = ctx.request.body
+
+  try {
+    await auth.signIn(email, password)
+
+    ctx.status = 200
+    ctx.body = {}
+  } catch (err) {
+    ctx.status = 400
+    ctx.body = {
+      error: err.message
+    }
+  }
 }
 
-async function signUp (ctx) {
-  ctx.body = {}
+function signUp (ctx) {
+  ctx.status = 500
+  ctx.body = {
+    error: 'sign up not implement'
+  }
 }
 
-async function signOut (ctx) {
-  ctx.body = {}
+function signOut (ctx) {
+  ctx.status = 500
+  ctx.body = {
+    error: 'sign out not implement'
+  }
 }
 
 module.exports = {
