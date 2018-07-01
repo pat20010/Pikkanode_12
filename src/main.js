8b8b6b2ecf7dbabf7c6ab116a06b639f3a07e287
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-07-01 16:21:55 +0700

    update config cors

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ const fse = require('fs-extra')
 
 const app = new Koa()
 app.use(cors({
-  origin: 'http://localhost:8000',
-  allowedMethods: ['POST']
+  // origin: 'http://localhost:8000'
+  credentials: true
 }))
 
 const dirPathImage = path.join(process.cwd(), 'src', 'public', 'imagesUpload')
