b0e2dc30b033a4e56c06cccf0a619f6d8f8a4614
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-08-02 00:40:48 +0700

    update add password in connection db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2/promise')
 const pool = mysql.createPool({
   connectionLimit: 5,
   user: 'root',
+  password: 'admin1',
   host: 'localhost',
   database: 'pikkanode'
 })
