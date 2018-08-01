765adb83bab83ecb9ea51d0c977a89042c852bb8
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-08-02 01:02:28 +0700

    add route use path pikka

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -4,5 +4,6 @@ const koaBody = require('koa-body')
 const router = new Router()
 
 router.use('/auth', koaBody(), require('./auth'))
+router.use(koaBody(), require('./pikka'))
 
 module.exports = router.routes()
