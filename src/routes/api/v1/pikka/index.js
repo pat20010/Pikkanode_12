2fe2a464b48b3030db42c5abf987075a8a1413ed
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-08-04 01:44:39 +0700

    use to pikka controller

--- src/routes/api/v1/pikka/index.js
+++ src/routes/api/v1/pikka/index.js
@@ -1,16 +1,10 @@
 const Router = require('koa-router')
+const koaBody = require('koa-body')
 const pikka = require('../../../../controllers/api/pikka')
 
 const router = new Router()
 
 router.get('/pikka', pikka.getPicturePikka)
-
-router.post('/pikka', ctx => {
-  ctx.status = 200
-  ctx.body = {
-    id: '2e33875e-a508-450e-a428-433a6ffd9ed6',
-    createdAt: '2018-06-19T10:33:12.000Z'
-  }
-})
+router.post('/pikka', koaBody(), pikka.postPicturePikka)
 
 module.exports = router.routes()
