c617307ffe6c5a9de838493c1ff684b6bc1727b2
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-08-04 01:29:27 +0700

    update fuction controller method get and post pikka

--- src/controllers/api/pikka.js
+++ src/controllers/api/pikka.js
@@ -1,24 +1,45 @@
 const { pictures } = require('../../models')
+const pikka = require('../../service/pikka')
 
 async function getPicturePikka (ctx) {
   try {
-    const pictureData = await pictures.getPictureCommentLikeData()
-    console.log(pictureData)
+    const pictureDatas = await pictures.getPictureCommentLikeData()
+    console.log(pictureDatas)
 
     ctx.status = 200
     ctx.body = {
-      list: [
+      list: pictureDatas.map(picture => (
         {
-          id: pictureData[0].id,
-          caption: pictureData[0].caption,
-          picture: `http://localhost:3000/-/images/${pictureData[0].id}`,
-          createdAt: pictureData[0].created_at,
-          commentCount: pictureData[0].comment_count,
+          id: picture.id,
+          caption: picture.caption,
+          picture: `http://localhost:8000/-/imagesUpload/${picture.id}.jpg`,
+          createdAt: picture.created_at,
+          commentCount: picture.comment_count,
           liked: true,
-          likeCount: pictureData[0].like_count,
-          createdBy: pictureData[0].created_by
+          likeCount: picture.like_count,
+          createdBy: picture.created_by
         }
-      ]
+      ))
+    }
+  } catch (err) {
+    ctx.status = 400
+    ctx.body = {
+      error: err.message
+    }
+  }
+}
+
+async function postPicturePikka (ctx) {
+  try {
+    const pictureId = pikka.uploadPicture(
+      ctx.request.body.caption,
+      ctx.request.files.picture,
+      ctx.session.userId
+    )
+
+    ctx.status = 200
+    ctx.body = {
+      id: pictureId
     }
   } catch (err) {
     ctx.status = 400
@@ -29,5 +50,6 @@ async function getPicturePikka (ctx) {
 }
 
 module.exports = {
-  getPicturePikka
+  getPicturePikka,
+  postPicturePikka
 }
