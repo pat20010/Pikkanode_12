6b2021aed05d4886dc5d83959839df9f52e43b29
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-08-04 16:20:55 +0700

    fix bug remove koabody in route pikka

--- src/routes/api/v1/pikka/index.js
+++ src/routes/api/v1/pikka/index.js
@@ -1,10 +1,9 @@
 const Router = require('koa-router')
-const koaBody = require('koa-body')
 const pikka = require('../../../../controllers/api/pikka')
 
 const router = new Router()
 
 router.get('/pikka', pikka.getPicturePikka)
-router.post('/pikka', koaBody(), pikka.postPicturePikka)
+router.post('/pikka', pikka.postPicturePikka)
 
 module.exports = router.routes()
