1466eb05735b0ac12dbb2aff4327267495245903
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-08-04 16:22:30 +0700

    add check authen api can using after login only

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -3,7 +3,19 @@ const koaBody = require('koa-body')
 
 const router = new Router()
 
+const chackAuthen = async (ctx, next) => { // Authen middleware
+  if (!ctx.session || !ctx.session.userId) {
+    ctx.status = 401
+    ctx.body = {
+      error: 'unauthorized'
+    }
+    return
+  }
+  await next()
+}
+
 router.use('/auth', koaBody(), require('./auth'))
-router.use(koaBody(), require('./pikka'))
+router.use(chackAuthen)
+router.use(koaBody({ multipart: true }), require('./pikka'))
 
 module.exports = router.routes()
